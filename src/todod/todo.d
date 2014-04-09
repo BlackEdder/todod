681a453013bccf413cfabbd1f9ce5029a627d46d
BlackEdder <edwin@tkwsping.nl>
2014-04-09 17:03:11 +0100

    Started working on writing and reading todo list

--- src/todod/todo.d
+++ src/todod/todo.d
@@ -3,30 +3,57 @@ module todod.todo;
 import std.string;
 import std.regex;
 
-struct todo {
+import std.json;
+
+import std.stdio;
+import std.file;
+
+struct Todo {
 	string title;
 }
 
 unittest {
-	todo t1;
+	Todo t1;
 	t1.title = "Todo 1";
 	assert( t1.title == "Todo 1" );
 }
 
-alias todo[] todos;
+JSONValue toJSON( const Todo t ) {
+	JSONValue[string] jsonTODO;
+	jsonTODO["title"] = t.title;
+	return JSONValue( jsonTODO );	
+}
+
+Todo toTodo( const JSONValue json ) {
+	Todo t;
+	t.title = json["title"].str;
+	return t;
+}
 
 unittest {
-	todo t1;
+	Todo t1;
 	t1.title = "Todo 1";
-	todo t2;
+	assert( toJSON( t1 ).toTodo.title == "Todo 1" );
+}
+
+/**
+	Working on list of todos
+	*/
+
+alias Todo[] Todos;
+
+unittest {
+	Todo t1;
+	t1.title = "Todo 1";
+	Todo t2;
 	t2.title = "Bla";
-	todos mytodos = [t1, t2];
+	Todos mytodos = [t1, t2];
 
 	assert(	mytodos[0].title == "Todo 1" );
 }
 
-todos search_title( const todos ts, string search ) {
-	todos answer;
+Todos search_title( const Todos ts, string search ) {
+	Todos answer;
 	foreach ( t; ts ) {
 		if (matchFirst( t.title.toLower, search.toLower ))
 		answer ~= t;
@@ -35,11 +62,11 @@ todos search_title( const todos ts, string search ) {
 }
 
 unittest {
-	todo t1;
+	Todo t1;
 	t1.title = "Todo 1";
-	todo t2;
+	Todo t2;
 	t2.title = "Bla";
-	todos mytodos = [t1, t2];
+	Todos mytodos = [t1, t2];
 
 	auto s = search_title( mytodos, "Bla" );
 
@@ -50,5 +77,43 @@ unittest {
 
 	assert(	s.length == 1 );
 	assert(	s[0].title == "Bla" );
+}
 
+JSONValue toJSON( const Todos ts ) {
+	JSONValue[] jsonTODOS;
+	foreach (t; ts) 
+		jsonTODOS ~= toJSON( t );
+	JSONValue[string] json;
+	json["todos"] = jsonTODOS;
+	return JSONValue( json );	
+}
+
+Todos toTodos( const JSONValue json ) {
+	Todos ts;
+	foreach ( js; json["todos"].array )
+		ts ~= toTodo( js );
+	return ts;
+}
+
+unittest {
+	Todo t1;
+	t1.title = "Todo 1";
+	Todo t2;
+	t2.title = "Bla";
+	Todos mytodos = [t1, t2];
+	assert(	toJSON(mytodos).toTodos[0].title == "Todo 1" );
+}
+
+Todos loadTodos() {
+	Todos ts;
+	if (exists(".todod.yaml" )) {
+		File file = File( ".todod.yaml", "r" );
+		ts = toTodos( JSONValue( file.readln() ) );
+	}
+	return ts;
+}
+
+void writeTodos( const Todos ts ) {
+	File file = File( ".todod.yaml", "w" );
+	file.write( toJSON(ts).str );
 }
