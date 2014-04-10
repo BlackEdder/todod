a80562a318023363d9a89ecd248ee184a4cba6f9
BlackEdder <edder@tkwsping.nl>
2014-04-10 09:54:21 +0100

    Move executable compiling to own configuration in dub

--- src/todo.d
+++ src/todo.d
@@ -1,32 +1,45 @@
 import std.stdio;
 import std.conv;
+import std.string;
+import std.algorithm;
 
 import todod.todo;
+
+Todos handle_message( string command, string parameter, Todos ts ) {
+	switch (command) {
+		case "add":
+			Todo t;
+			t.title = parameter;
+			ts ~= t;
+			break;
+		case "del":
+			size_t id = to!size_t(parameter);
+			if (id < ts.length - 1 )
+				ts = ts[0..id] ~ ts[id+1..$];
+			else if (id == ts.length - 1)
+				ts = ts[0..id];
+			break;
+		case "show":
+			if (ts.length == 0)
+				writeln( "No todos yet. Add them using todod add [TODO]" );
+			else
+				write( toString( ts ) );
+			break;
+		default:
+			writeln( "Unknown option" );
+			break;
+	}
+	return ts;
+}
 
 void main( string[] args ) {
 	scope( exit ) { writeTodos( ts ); }
 	auto ts = loadTodos();
 
-	if ( args.length >= 2 ) {
-		switch (args[1]) {
-			case "add":
-				Todo t;
-				t.title = args[2];
-				ts ~= t;
-				break;
-			case "del":
-				size_t id = to!size_t(args[2]);
-				if (id < ts.length)
-					ts = ts[0..id] ~ ts[id+1..$];
-				break;
-			default:
-				writeln( "Unknown option" );
-				break;
-		}
-	} else {
-		if (ts.length == 0)
-			writeln( "No todos yet. Add them using todod add [TODO]" );
-		else
-			write( toString( ts ) );
+	bool quit = false;
+	while (!quit) {
+		write( "> " );
+		auto commands = readln().chomp().findSplit( " " );
+		ts = handle_message( commands[0], commands[2], ts );
 	}
 }
