369307cf5a8d132895fa5c71ea798a9d0ab3a250
BlackEdder <edder@tkwsping.nl>
2014-04-10 08:31:07 +0100

    Script that runs test automatically

---
+++ autotest.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Immediately run once
+dub test
+
+trap "dub test" INT
+
+function watch_tests() {
+while : 
+do
+	#file=`inotifywait -q -e CREATE bin/ --format %f`
+	file=`inotifywait -q -e ATTRIB src/todod/ --format %f`
+	dub test
+done
+}
+
+watch_tests
