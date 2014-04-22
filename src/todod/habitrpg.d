e0ae1a516faf9b370ae2da3ab1f316689a12561b
BlackEdder <edder@tkwsping.nl>
2014-04-22 12:58:29 +0100

    Working example

--- src/todod/habitrpg.d
+++ src/todod/habitrpg.d
@@ -1,6 +1,11 @@
 module todod.habitrpg;
 
 import std.net.curl;
+import std.conv;
+import std.algorithm;
+import std.array; 
+import core.thread;
+import std.encoding;
 
 version (unittest) {
 	import std.stdio;
@@ -10,8 +15,25 @@ string user_id = "f55f430e-36b8-4ebf-b6fa-ad4ff552fe7e";
 string api_token = "3fca0d72-2f95-4e57-99e5-43ddb85b9780";
 
 unittest {
-  auto content = post("https://beta.habitrpg.com/api/v2/user/tasks/productivity/up", "" ); //x-api-key: 3fca0d72-2f95-4e57-99e5-43ddb85b9780;x-api-user: f55f430e-36b8-4ebf-b6fa-ad4ff552fe7e");
-	writeln( content );
+	auto http = HTTP( "https://habitrpg.com/api/v2/user/tasks/productivity/up" );
+	http.addRequestHeader( "x-api-user", user_id );
+	http.addRequestHeader( "x-api-key", api_token );
+	http.addRequestHeader( "Content-Type","application/json" );
+	http.postData = "";
+	http.method = HTTP.Method.post;
+	http.verbose( true );
+
+	string result;
+	http.onReceive = (ubyte[] data) { 
+		auto result = array( map!(a => cast(char) a)( data ) );
+		writeln( result );
+
+		return data.length; };
+
+	http.perform();
+
+	writeln( result );
+
 	assert( true );
 }
 
