45412249075611a8da321c9c69747708bf78dde0
BlackEdder <edder@tkwsping.nl>
2014-05-13 09:32:52 +0100

    Further work on adding a commit

--- src/todod/storage.d
+++ src/todod/storage.d
@@ -23,13 +23,31 @@ unittest {
 	file.writeln( randomUUID.toString );
 
 	git_repository_index(&my_repo_index, repo);
-	git_index_add_bypath(my_repo_index,"a.txt");
-	git_index_write(my_repo_index);
+	//git_index_add_bypath(my_repo_index,(repoPath ~ "a.txt").toStringz);
+	git_index_add_bypath(my_repo_index,("a.txt").toStringz);
+	//git_index_write(my_repo_index);
+
+	git_oid tree_id, commit_id;
+	git_tree *tree;
+	git_index_write_tree(&tree_id, my_repo_index);
+	git_index_write( my_repo_index );
 
 	git_signature *sig;
 	enforce( git_signature_default(&sig, repo) >= 0 );
 
+	git_tree_lookup(&tree, repo, &tree_id);
 
+	git_commit* old_head;
+	git_revparse_single( cast(git_object**)&old_head,
+                                 repo, "HEAD" );
+
+	enforce( git_commit_create_v(
+            &commit_id, repo, "HEAD", sig, sig,
+            null, "Initial commit", tree, old_head) >=0 );
+
+	// Free everything
+	git_tree_free(tree);
+	git_index_free(my_repo_index);
 	git_signature_free(sig);
 	git_repository_free(repo);
 	git_threads_shutdown();
