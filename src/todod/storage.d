c5dc92573c6bcdadf39d0d10c84a67b1ac2bb6db
BlackEdder <edder@tkwsping.nl>
2014-05-13 11:01:11 +0100

    Correctly lookup parent ids

--- src/todod/storage.d
+++ src/todod/storage.d
@@ -16,39 +16,56 @@ unittest {
 	
 	auto repoPath = "/home/edwin/tmp/test_libgit2/";
 
-	git_threads_init();
+	//git_threads_init();
 	enforce( git_repository_init(&repo, repoPath.toStringz, 0) >= 0 );
+	//enforce( git_repository_open(&repo, repoPath.toStringz) >= 0 );
+
+	string uuid = randomUUID.toString;
+	writeln( "UUID ", uuid );
 	
 	File file = File( repoPath ~ "a.txt", "w" );
-	file.writeln( randomUUID.toString );
-
-	git_repository_index(&my_repo_index, repo);
-	//git_index_add_bypath(my_repo_index,(repoPath ~ "a.txt").toStringz);
-	git_index_add_bypath(my_repo_index,("a.txt").toStringz);
-	//git_index_write(my_repo_index);
+	file.writeln( uuid );
 
 	git_oid tree_id, commit_id;
 	git_tree *tree;
-	git_index_write_tree(&tree_id, my_repo_index);
-	git_index_write( my_repo_index );
+	//git_index_read_tree(my_repo_index, tree);
+
+	enforce( git_repository_index(&my_repo_index, repo) >= 0 );
+	//git_index_add_bypath(my_repo_index,(repoPath ~ "a.txt").toStringz);
+	enforce( git_index_add_bypath(my_repo_index,("a.txt").toStringz) >= 0 );
+	//git_index_write(my_repo_index);
+
+
+	enforce( git_index_write( my_repo_index ) >= 0 );
+	enforce( git_index_write_tree(&tree_id, my_repo_index) >= 0 );
+	enforce( git_tree_lookup(&tree, repo, &tree_id) >= 0, "Tree lookup failed" );
 
 	git_signature *sig;
 	enforce( git_signature_default(&sig, repo) >= 0 );
 
-	git_tree_lookup(&tree, repo, &tree_id);
 
+	/*git_reference *head;
+	git_repository_head(&head, repo);*/
 	git_commit* old_head;
-	git_revparse_single( cast(git_object**)&old_head,
-                                 repo, "HEAD" );
+	int rc = git_revparse_single( cast(git_object**) &old_head,
+			repo, "HEAD" );
 
-	enforce( git_commit_create_v(
-            &commit_id, repo, "HEAD", sig, sig,
-            null, "Initial commit", tree, old_head) >=0 );
+	if (rc<0) { // no head
+		debug writeln( "No head" );
+		enforce( git_commit_create_v(
+					&commit_id, repo, "HEAD", sig, sig,
+					"UTF-8", "Initial commit", tree, 0 ) >=0 );
+	}
+	else
+		enforce( git_commit_create_v(
+					&commit_id, repo, "HEAD", sig, sig,
+					"UTF-8", "Followup commit", tree, 1, old_head) >=0 );
 
 	// Free everything
-	git_tree_free(tree);
+	git_commit_free( old_head );
 	git_index_free(my_repo_index);
 	git_signature_free(sig);
+	git_tree_free(tree);
 	git_repository_free(repo);
-	git_threads_shutdown();
+	//git_threads_shutdown();
 }
