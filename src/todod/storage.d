9bcb5da7c2b460daa81c0167baf72dfbfe9b932d
BlackEdder <edder@tkwsping.nl>
2014-05-13 08:39:07 +0100

    Creating signature

--- src/todod/storage.d
+++ src/todod/storage.d
@@ -1,7 +1,8 @@
 module todod.storage;
 
-import std.uuid;
+import std.exception;
 import std.string;
+import std.uuid;
 
 import deimos.git2.all;
 
@@ -16,7 +17,7 @@ unittest {
 	auto repoPath = "/home/edwin/tmp/test_libgit2/";
 
 	git_threads_init();
-	git_repository_init(&repo, repoPath.toStringz, 0);
+	enforce( git_repository_init(&repo, repoPath.toStringz, 0) >= 0 );
 	
 	File file = File( repoPath ~ "a.txt", "w" );
 	file.writeln( randomUUID.toString );
@@ -24,4 +25,12 @@ unittest {
 	git_repository_index(&my_repo_index, repo);
 	git_index_add_bypath(my_repo_index,"a.txt");
 	git_index_write(my_repo_index);
+
+	git_signature *sig;
+	enforce( git_signature_default(&sig, repo) >= 0 );
+
+
+	git_signature_free(sig);
+	git_repository_free(repo);
+	git_threads_shutdown();
 }
